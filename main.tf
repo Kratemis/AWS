bc921af0c7f9472e55bef356f992e8aec7f906be
Sergio Esteban Adán <sergio@nttdata.com>
2024-04-28 11:51:08 +0200

    Disable step function module

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 # Get account_id with: data.aws_caller_identity.current.account_id
 data "aws_caller_identity" "current" {}
 
-module "step_function" {
-  source = "./examples/step_function"
+# module "step_function" {
+#   source = "./examples/step_function"
 
-  region = var.region
-  account_id = data.aws_caller_identity.current.account_id
-  tags = {
-    Example = "step_functions"
-  }
-}
+#   region = var.region
+#   account_id = data.aws_caller_identity.current.account_id
+#   tags = {
+#     Example = "step_functions"
+#   }
+# }
